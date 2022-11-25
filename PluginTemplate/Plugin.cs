efc399bb7437a63f8a23a7b157f575755ecb54f4
Dekay <43781572+Dekay21@users.noreply.github.com>
2022-11-25 20:20:51 +0100

    Initial commit

---
+++ PluginTemplate/Plugin.cs
@@ -0,0 +1,59 @@
+﻿using Terraria;
+using TerrariaApi.Server;
+
+namespace PluginTemplate
+{
+    [ApiVersion(2, 1)]
+    public class Plugin : TerrariaPlugin
+    {
+        /// <summary>
+        /// The name of the plugin.
+        /// </summary>
+        public override string Name => "PluginTemplate";
+
+        /// <summary>
+        /// The version of the plugin in its current state.
+        /// </summary>
+        public override Version Version => new(1, 0, 0);
+
+        /// <summary>
+        /// The author(s) of the plugin.
+        /// </summary>
+        public override string Author => "Dekay";
+
+        /// <summary>
+        /// A short, one-line, description of the plugin's purpose.
+        /// </summary>
+        public override string Description => "Template to create new Tshock Plugins";
+
+        /// <summary>
+        /// The plugin's constructor
+        /// Set your plugin's order (optional) and any other constructor logic here
+        /// </summary>
+        public Plugin(Main game) : base(game)
+        {
+        }
+
+        /// <summary>
+        /// Performs plugin initialization logic.
+        /// Add your hooks, config file read/writes, etc here
+        /// </summary>
+        public override void Initialize()
+        {
+            // Initialize here
+        }
+
+        /// <summary>
+        /// Performs plugin cleanup logic
+        /// Remove your hooks and perform general cleanup here
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // Dispose here
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
